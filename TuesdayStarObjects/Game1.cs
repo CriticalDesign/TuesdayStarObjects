41fbf93cade5e264572e0fc4040d81e37beca36e
Aaron Langille <aaron.langille@gmail.com>
2023-11-28 16:25:56 -0500

    Add project files.

---
+++ TuesdayStarObjects/Game1.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace TuesdayStarObjects
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private List<Star> myStar;
+        private Texture2D _starTexture;
+        private int _numStars, _numStarsOnScreen;
+        private SpriteFont _gameFont;
+
+        private bool _notSpacePressed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _numStars = 50;
+            _numStarsOnScreen = 0;
+            _notSpacePressed = true;
+            myStar = new List<Star>();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _starTexture = Content.Load<Texture2D>("Star_Sprite");
+
+            for (int i = 0; i < _numStars; i++)
+            {
+                myStar.Add(new Star(_starTexture));
+            }
+
+            _gameFont = Content.Load<SpriteFont>("GameFont");
+            
+            
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && _notSpacePressed)
+            {
+                _notSpacePressed = false;
+                _numStars++;
+            }
+
+            if (!Keyboard.GetState().IsKeyDown(Keys.Space) && !_notSpacePressed)
+                _notSpacePressed = true;
+
+
+            for (int i = 0; i < myStar.Count; i++)
+            {
+                myStar[i].Update();
+            }
+
+            _numStarsOnScreen = 0;
+            for(int i = 0; i < myStar.Count; i++)
+            {
+                if (myStar[i].GetY() > 0 && myStar[i].GetY() < 480)
+                    _numStarsOnScreen++;
+
+                if (myStar[i].GetY() > 600)
+                    myStar.RemoveAt(i);
+            }
+
+            while(myStar.Count < _numStars)
+            {
+                myStar.Add(new Star(_starTexture));
+            }
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            for (int i = 0; i < myStar.Count; i++)
+            {
+                myStar[i].Draw(_spriteBatch);
+            }
+
+
+            _spriteBatch.Begin();
+            _spriteBatch.DrawString(_gameFont, "Number of stars total: " + _numStars, new Vector2(10,10), Color.White);
+            _spriteBatch.DrawString(_gameFont, "Number of stars on screen: " + _numStarsOnScreen, new Vector2(450, 10), Color.White);
+
+            _spriteBatch.DrawString(_gameFont, "Press \"space\" to add another star.", new Vector2(240, 450), Color.White);
+
+
+            _spriteBatch.End();
+
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TuesdayStarObjects/Star.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace TuesdayStarObjects
+{
+    internal class Star
+    {
+
+        private Texture2D _starTexture;
+        private Vector2 _starPosition;
+        private Vector2 _starScale;
+        private float _starRotation;
+        private Vector2 _starOrigin;
+        private Color _starColor;
+        private Random _rng;
+
+        private float _starSpeed, _starRotationSpeed;
+
+        public Star(Texture2D starTextureIn)
+        {
+            _rng = new Random();
+
+
+            _starSpeed = _rng.Next(50, 151) / 100f;
+            _starRotationSpeed = _rng.Next(-100, 101) / 2500f;
+
+            _starTexture = starTextureIn;
+            _starPosition = new Vector2(_rng.Next(0, 801), _rng.Next(-400, -100));
+
+
+            float scaleTemp = _rng.Next(20, 56) / 100f;
+            _starScale = new Vector2(scaleTemp, scaleTemp);
+            
+            _starRotation = _rng.Next(1,101) / 100f;
+            _starColor = new Color(128 + _rng.Next(1, 129), 128 + _rng.Next(1, 129), 128 + _rng.Next(1, 129));
+            _starOrigin = new Vector2(_starTexture.Width / 2f, _starTexture.Height / 2f);
+        }
+
+        public void Update()
+        {
+            _starPosition.Y += _starSpeed;
+            _starRotation += _starRotationSpeed;
+        }
+
+
+        public void Draw(SpriteBatch starSpriteBatch)
+        {
+            starSpriteBatch.Begin();
+            starSpriteBatch.Draw(_starTexture,
+                _starPosition,
+                null,
+                _starColor,
+                _starRotation,
+                _starOrigin,
+                _starScale,
+                SpriteEffects.None,
+                0
+                );
+            starSpriteBatch.End();
+        }
+
+        public float GetY()
+        {
+            return _starPosition.Y;
+        }
+
+    }
+}
